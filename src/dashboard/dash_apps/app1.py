ff456757849a4412cd270effd234e2731afe54fc
DevIA.E15 <DevIA.E15@afpar.com>
2022-12-12 11:55:44 +0400

    ajout d'un top pour les graphique

--- src/dashboard/dash_apps/app1.py
+++ src/dashboard/dash_apps/app1.py
@@ -4,6 +4,9 @@ from django_plotly_dash import DjangoDash
 import pandas as pd
 import dash_bootstrap_components as dbc
 from dashboard.models import Produit, Pays, Facture, Detail, Annee, Histopays, Histoproduit 
+import math
+import plotly.graph_objects as go
+import dash_html_components as dhtml
 
 
 bootstrap_theme=[dbc.themes.BOOTSTRAP,'https://use.fontawesome.com/releases/v5.9.0/css/all.css']
@@ -18,89 +21,107 @@ histoPays['histo'] = 'pays'
 
 df = pd.concat([histoProduit,histoPays])
 
-
 app = DjangoDash('SimpleExample',external_stylesheets=bootstrap_theme )
-
-
-import plotly.graph_objects as go
 
 fig = go.Figure()
 
 app.layout = html.Div([
 
     html.Div([
+            dhtml.Div(id='output'),
             dcc.Dropdown(
                 df['histo'].unique(),
                 'produit',
                 id='histo-input'
             )], style={'width': '48%', 'display': 'inline-block'}),
 
+    html.Div([
+        dcc.Dropdown(
+            'top 0 a 20',
+             0,
+            id='top-input'
+        )], style={'width': '48%', 'display': 'inline-block'}),
+
 
     html.Div([dcc.Graph(id='bar_graph',figure=fig)]),
 
     dcc.Slider(
-        df['annee'].min(),
-        df['annee'].max(),
         step=None,
-        id='year--slider',
-        value=df['annee'].max(),
+        id='year-slider',
+        value=df['annee'].min(),
         marks={str(year): str(year) for year in df['annee'].unique()},
 
     )
 
-
+#Region.objects.annotate(invoice_details = Count('invoice__details')).order_by('-invoice_details')
              
 ])
+
 # https://plotly.com/python/click-events/ ON CLICK
 @app.callback(
 
     Output('bar_graph', 'figure'),
     Input('histo-input', 'value'),
-    Input('year--slider', 'value')
+    Input('top-input', 'value'),
+    Input('year-slider', 'value')
     )
-def update_bar(histo_input,year_value):
+def update_bar(histo_input,top_input,year_value):
 
-    print(year_value)
-    print(type(year_value))
+    # select donnée selon l'année
+    dff = df.loc[df['annee'] == str(year_value)]
+ 
+    # select donnée selon valeur du dropdown
+    qtproduit = dff.loc[dff['histo'] == histo_input]
+
+    # classe les donnée dans l'ordre decroissant selon la quantité
+    qtproduit.sort_values(by=['quantite'], inplace=True, ascending=False)
+
+    # determine l'interval que l'on veut regardé
+    topDebut = top_input * 20
+    topFin = (top_input + 1) * 20
+    qtproduit = qtproduit.iloc[topDebut : topFin,:]
+
+    # construit le graphique
+    fig = go.Figure(go.Bar(x=qtproduit['indicator'],
+                            y=qtproduit['quantite'],
+                            marker_color = 'green',
+                            
+                            ),
+                    go.Layout(  title='Nombre commande par '+ histo_input +'',
+                                title_x = 0.5, 
+                            ),
+
+                    )
+
+    fig.update_layout(barmode='stack', bargap=0.1,bargroupgap=0.1)
+
+    return fig
+
+
+# Permet de mettre a jour le dropdown "top"
+@app.callback(
+    Output('top-input', 'options'),
+    Input('histo-input', 'value'),
+    Input('year-slider', 'value')
+    )
+def update_dropdown(histo_input,year_value):
+    
+    # select donnée selon l'année
     dff = df.loc[df['annee'] == str(year_value)]
 
-    print(dff)
-    #Magnitude vs Number
-    if histo_input == "pays": 
-        print(dff)
-        qtproduit = dff.loc[dff['histo'] == "pays"]
-        print(qtproduit)
+    # select donnée selon valeur du dropdown
+    qtproduit = dff.loc[dff['histo'] == histo_input]
 
-        fig = go.Figure(go.Bar(x=qtproduit['indicator'],
-                               y=qtproduit['quantite'],
-                               marker_color = 'green',
-                               
-                               ),
-                        go.Layout(  title='Nombre commande par pays',
-                                    title_x = 0.5, 
-                                    xaxis=dict( tickangle=90,
-                                                tickmode='linear',
-                                                tick0=0.1,
-                                                dtick=0.1
-                                    )),
+    # dertermine le nombre de partition
+    nbDec = 20
+    nbPart = math.ceil(len(qtproduit) / nbDec)
 
-                        )
-        fig.update_layout(barmode='stack', bargap=0.1,bargroupgap=0.1)
+    #retourne les option du dropdown
+    return [{'label': 'top '+ str(i *20) + ' à top ' + str((i+1)*20) +'', 'value': i} for i in range(int(nbPart))]
 
-    #Depth vs Number
-    elif histo_input == "produit": 
-        
-        qtproduit = dff.loc[dff['histo'] == "produit"]
 
-        fig = go.Figure(go.Bar(x=qtproduit['indicator'],
-                               y=qtproduit['quantite'],
-                               marker_color = 'blue',
-                               
-                               )
-                        )
 
-    else:
-        pass
+
+
     
-    return fig
-        
+
